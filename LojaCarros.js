db161112119e25d274d4b0152900365cc332b321
Facundo <facundo@MacBook-Pro-de-Mac-2.local>
2023-02-25 22:48:46 -0300

    persist and dom

--- LojaCarros.js
+++ LojaCarros.js
@@ -1,23 +1,3 @@
-// Ex 1 - Crie uma função para cadastrar Marca de Veículos
-
-// Ex 2 - Crie uma função para cadastrar Modelo de Veículos
-
-// Ex 3 - Crie função para cadastrar Veículo
-
-// Ex 4 - Crie uma função para listar os veículos cadastrados.
-
-// Ex 5 - Crie uma página somente com um menu com as seguintes opcões (Início, Marca, Modelo, Veículo)
-
-// Ex 6 - Crie uma página para cadastro de Marca de veículos
-
-// Ex 7 - Crie uma página para cadastro de Modelo de veículos
-
-// Ex 8 - Crie uma página para cadastro de veículo
-
-// Ex 9 - Crie uma página index para mostrar a listagem dos veículos
-
-// Ex 10 - Persistir informações no Localstorage e algumas melhorias
-
 var marcas = []; 
 
 function cadastrarMarca(){
@@ -51,7 +31,6 @@ var veiculos = [];
 
 function cadastrarVeiculo(){
     const veiculo = {};
-    // veiculo.marca = document.getElementById("cars").value;
     veiculo.modelo = document.getElementById("cars").value;
     veiculo.imagem = document.getElementById("imagem").src;
     veiculo.cor = document.getElementById("cor").value;
@@ -60,28 +39,50 @@ function cadastrarVeiculo(){
     veiculos.push(veiculo);
     console.log('Veiculo cadastrado com suceso!');
     console.log(veiculo);
+    let veiculosJSON = JSON.stringify(veiculos);
+    localStorage.setItem("veiculos",veiculosJSON);
 };
 
-// cadastrarVeiculo("Chevrolet", "Onix", "Localiza", "30000", "40000","branco");
+//cadastrarVeiculo("Chevrolet", "Onix", "Localiza", "30000", "40000","branco");
 // cadastrarVeiculo("Ford", "Festa", "RentCars", "500000", "30000","cinza");
 // cadastrarVeiculo("Renault", "Kwid", "eletrico", "0", "150000","rosa");
 // console.log(veiculos);
 
-var objVeiculo = {
-    Marca:"",Modelo:"",Veiculo:"",km:"",valor:"",cor:"",
-}
 
-function listarVeiculo(){
-    for (let v in veiculos){
-        let veiculo= veiculos[v];
-        objVeiculo.Marca=veiculo[0];
-        objVeiculo.Modelo=veiculo[1];
-        objVeiculo.Veiculo=veiculo[2];
-        objVeiculo.km=veiculo[3];
-        objVeiculo.valor=veiculo[4];
-        objVeiculo.cor=veiculo[5];
-        console.log(objVeiculo);
-        return objVeiculo;
+
+// function listarVeiculos(){
+//     let veiculosJSON = document.getElementById("veiculos");
+//     let veiculosLista = JSON.parse(veiculosJSON);
+//     let objVeiculo = {
+//         Marca:"",Modelo:"",Veiculo:"",km:"",valor:"",cor:"",
+//     }
+//     for (let v in veiculosLista){
+//         let veiculoLista= veiculosLista[v];
+//         objVeiculo.Marca=veiculoLista[0];
+//         objVeiculo.Modelo=veiculoLista[1];
+//         objVeiculo.Veiculo=veiculoLista[2];
+//         objVeiculo.km=veiculoLista[3];
+//         objVeiculo.valor=veiculoLista[4];
+//         objVeiculo.cor=veiculoLista[5];
+//         console.log(objVeiculo);
+//         var card = document.createElement("div");
+//         var descricao = document.creatElement("p");
+//         document.getElementById("listaCarros").appendChild(card);
+//         card.appendChild(descricao);
+//     }
+// }
+
+function listarVeiculos(){
+    let veiculosJSON = localStorage.getItem("veiculos");
+    let veiculosParse = JSON.parse(veiculosJSON);
+    console.log(veiculosJSON);
+    console.log(veiculosParse);
+    for (let veiculo in veiculosParse){
+       let objVeiculo = (veiculosParse[veiculo]);
+       let card = document.createElement("div");
+       let descricao = document.createElement("p");
+       card.appendChild(descricao);
+       document.getElementById("listaCarros").appendChild(card);
     }
 }
-listarVeiculo();
+;
